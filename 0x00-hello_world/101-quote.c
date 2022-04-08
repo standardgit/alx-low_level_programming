35c823140162b11077076ef0a2b1d5086b17cc17
standardgit <standardblaise@gmail.com>
2022-04-08 16:42:52 -0700

    Deadline passed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main -Prints 
+*Returns
+*
+*/
+int main(void)
+{
+write(2, "and that peice of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+return (1);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing function
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
