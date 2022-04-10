fb1f6f053c86394e2d6858fdb89908d30554dc3d
standardgit <64153142+standardgit@users.noreply.github.com>
2022-04-11 00:47:40 +0100

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 after function run
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+printf("\n");
+return (0);
+}
