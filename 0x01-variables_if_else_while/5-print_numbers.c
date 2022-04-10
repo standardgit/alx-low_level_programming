fe672048e4b81055e1644038336df2c13f9ece64
standardgit <64153142+standardgit@users.noreply.github.com>
2022-04-11 00:52:22 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - enter point
+ *
+ * Return: 0 after function
+ */
+int main(void)
+{
+int num = 0;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
