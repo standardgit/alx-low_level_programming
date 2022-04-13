f42023d13d09f1c047345ecd4dbf9751bbf29c0b
standardgit <64153142+standardgit@users.noreply.github.com>
2022-04-14 00:57:09 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,19 @@
-#include "stdio.h"
-#include "main.h"
+#include <stdio.h>
 /**
+ * main - entry
+ *
+ * Return: 0 after run
+ */
+void print_alphabet_x10(void)
+{
+  int n, i;
+  i = 0;
+  while (i < 10)
+    for (n = 'a'; n <= 'z'; n++)
+    {
+      -putchar(n)
+      }
+      i++;
+      _putchar('\n');
+  }
+}
