695ce6c262925abbb75bf3e036588a6dd06f67f5
standardgit <standardblaise@gmail.com>
2022-04-21 06:19:21 -0700

    swap digit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - swap to int parameter
+ *
+ * @a: int parameter
+ * @b: int parameter
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int num;
+
+num = *a;
+*a = *b;
+*b = num
+}
