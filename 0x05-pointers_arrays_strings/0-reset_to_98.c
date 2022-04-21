08f70ddf136b4fca985bbbfd3414631a9841bac3
standardgit <standardblaise@gmail.com>
2022-04-21 06:00:20 -0700

    task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98.c - take pointer to an int parameter
+ * 
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointer string and array
