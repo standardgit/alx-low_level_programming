505208d46d8f23d029224a8d2fcbdb0c0c1ea0ce
standardgit <standardblaise@gmail.com>
2022-04-21 07:19:44 -0700

    get length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * _strlen - get the length of char
+ *
+ * @s: int parameter
+ *
+ * Return: int of s
+ */
+
+int _strlen(char *s)
+{
+int num;
+
+num = strlen(s)
+return (num)
+}
