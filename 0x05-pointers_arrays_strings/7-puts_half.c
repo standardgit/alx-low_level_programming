bef3f8b6e2254d462a7a627ef6f27901b848e65e
standardgit <standardblaise@gmail.com>
2022-04-21 11:32:48 -0700

    morere

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,14 @@ void puts_half(char *str)
 int i;
 int num;
 num = strlen(str);
-i = (strlen(str) - 1) / 2;
+if (num % 2 != 0)
+{
+i = strlen(str) - 1 / 2;
+}
+else
+{
+i = strlen(str) / 2;
+}
 while (i <= num - 1)
 {
 _putchar(str[i]);
