6eeb6ab8cb0ca9c3a8640129af7f0ea29a5c9741
standardgit <standardblaise@gmail.com>
2022-04-21 13:27:45 -0700

    more

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcyp - copy string
+ * @dest: char parameter
+ * @src: char parameter
+ * Return: char 
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcyp(dest, src);
+return (dest);
+}
