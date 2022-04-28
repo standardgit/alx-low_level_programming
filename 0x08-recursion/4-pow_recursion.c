0ba772ab70e9b7f62a847731ec463c3c7be3a75a
standardgit <standardblaise@gmail.com>
2022-04-28 13:33:49 -0700

    more

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include <math.h>
+
 
 /**
  * _pow_recursion - get power of a int
@@ -9,12 +9,17 @@
 
 int _pow_recursion(int x, int y)
 {
-int i;
-i = pow(x, y);
-if (i == 0)
+if (y < 0)
 {
 return (-1);
 }
-
-return (i);
+if (y == 0)
+{
+return (1);
+}
+if (y == 1)
+{
+return (x);
+}
+return (x * _pow_recursion(x, y - 1));
 }
