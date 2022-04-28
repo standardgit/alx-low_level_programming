61a6997b141a7eaf3efae1190a930aec4902098c
standardgit <standardblaise@gmail.com>
2022-04-28 13:45:22 -0700

    more

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print reverse
+ * @s: char param
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s+1);
+printf("%c", *s);
+}
+}
