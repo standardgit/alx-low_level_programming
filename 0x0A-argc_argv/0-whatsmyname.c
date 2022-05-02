02bf4ece5c006cdadb24eccfb8e8f9af6d823621
standardgit <standardblaise@gmail.com>
2022-05-02 16:34:04 -0700

    Task on argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+include "main.h"
+/**
+ * main - entry point
+ * @argc: int parameter
+ * @argv: char parameter
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n",  argv[0]);
+return (0);
+}
