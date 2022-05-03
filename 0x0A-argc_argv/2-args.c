8d64577e2c54984bfa61eeab093053382a478c7e
standardgit <64153142+standardgit@users.noreply.github.com>
2022-05-03 01:42:10 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,19 @@
-include <stdio.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - entry point
+ *@argc: int parameter
+ *@argv: char parameter
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+i = 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
