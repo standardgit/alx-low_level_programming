0c562158a4c33169c83a09d126f52d2a984ae757
standardgit <64153142+standardgit@users.noreply.github.com>
2022-05-09 20:33:06 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+void mul(int *product, char *n1, char *n2, int len1, int len2);
 
 #endif /* MAIN_H */
