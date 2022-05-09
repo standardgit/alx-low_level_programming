fb17d234c5280b171237ec78c74c876149cf9f59
standardgit <64153142+standardgit@users.noreply.github.com>
2022-05-09 20:18:47 +0100

    Update 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - entry point
+ * @b: unsigned int
+ * Return: void or memory or 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+char *s;
+
+s = malloc(b * sizeof(char));
+if (s == NULL)
+{
+exit(98);
+}
+return (s);
+}
