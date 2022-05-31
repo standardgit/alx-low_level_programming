a92384758ac50d8a8bfeaa9da8320c63f58a0a80
standardgit <64153142+standardgit@users.noreply.github.com>
2022-05-31 16:42:14 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * add- adds two integers and returns the result
+  * @a: one of the #s
+  * @b: the other #
+  * Return:  result
+  */
+int add(int a, int b)
+{
+return (a + b);
+}
