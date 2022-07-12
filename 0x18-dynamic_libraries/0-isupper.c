168639ff562ba9fe4eac7ca71d31e96406c3c30c
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-12 23:20:36 +0100

    Create 0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - check upper case
+ * @c: pint int
+ * Return: give 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
