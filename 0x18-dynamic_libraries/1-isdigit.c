a2e1a4b099b2cfbe1d07abf66d11ea7701c1a667
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-12 23:25:01 +0100

    Create 1-isdigit.c

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - check if it number
+ * @c: print int
+ * Return: give 1 or 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
