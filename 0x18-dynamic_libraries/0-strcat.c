2eb6efd8d8e153f600be05ae616c6fbf356b1d42
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-12 23:23:51 +0100

    Create 0-strcat.c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * *_strcat - more on it
+ * @dest: char parameter
+ * @src: char
+ * Return: char string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
