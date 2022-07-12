633967ae79951a702ae28afe21a39c006f601a69
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-12 23:31:35 +0100

    Create 3-strcmp.c

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strcmp - compare string
+ * @s1: char
+ * @s2: char
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int n;
+n = strcmp(s1, s2);
+return (n);
+}
