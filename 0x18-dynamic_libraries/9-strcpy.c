36bcec91be9a0eaeda18f840956555fd86b0da27
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-07-12 23:35:52 +0100

    Create 9-strcpy.c

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * *_strcpy - copy
+ * @dest: char paramter
+ * @src: char parameter
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
