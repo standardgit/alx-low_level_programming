d0fb05e267ee397aa156028e738ee0ef923c2301
Toluwalope <64153142+standardgit@users.noreply.github.com>
2022-11-03 00:51:46 +0100

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search through the numbers
+ * @array: is the array
+ * @value: is the value
+ * @size: is the size
+ * Return: void
+ */
+
+ int linear_search(int *array, size_t size, int value)
+ {
+	size_t i = 0;
+
+	if (array == NULL)
+	{
+		return(-1);
+	}
+	for(i = 0; i <= size - 1; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if(array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+ }
+ 
