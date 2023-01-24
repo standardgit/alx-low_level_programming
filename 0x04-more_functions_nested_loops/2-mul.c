d9fc1520fcabb51297d68017517b19478b22a7e3
standardgit <standardblaise@gmail.com>
2023-01-25 00:32:17 +0100

    Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two number
+ * @a: print int
+ * @b: print int
+ * Return: int
+ */
+int mul(int a, int b)
+{
+int answer;
+answer = a * b;
+return (answer);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * print_square - prints a hashtag square
+  * @size: how big the square is
+  *
+  *
+  * Return: void
+  */
+void print_square(int size)
+{
+int x;
+int y;
+
+if (size > 0)
+{
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
+else
+_putchar('\n');
+}
+
